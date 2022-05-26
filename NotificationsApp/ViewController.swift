56112ded2c08932182fa5fbe972a46e8a73dff0f
Felix Titov <titow.feliks@yandex.ru>
2022-05-26 12:42:59 +0300

    Initial Commit

---
+++ NotificationsApp/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  NotificationsApp
+//
+//  Created by Felix Titov on 5/26/22.
+//  Copyright © 2022 by Felix Titov. All rights reserved.
+//  
+
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
